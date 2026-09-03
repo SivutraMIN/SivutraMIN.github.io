d8296665b45b3ba57f548cef7fcf3d8287ef1b01
SivutraMIN <minsivutra@gmail.com>
2026-09-03 14:23:57 +0700

    fixed routing

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 const project_button = document.getElementById("btn-project")
 project_button.addEventListener("click", () => {
     try {
-        window.location.href = "./projects.html"
+        window.location.href = "../html/projects.html"
     }   
     catch (error) {
         console.error("File not Found!")
@@ -12,7 +12,7 @@ project_button.addEventListener("click", () => {
 const blog_button = document.getElementById("btn-blog")
 blog_button.addEventListener("click", () => {
     try {
-        window.location.href = "./blogs.html"
+        window.location.href = "../html/blogs.html"
     }
     catch (error) {
         console.error("File not Found!")
@@ -24,10 +24,10 @@ github_button.addEventListener("click", () => {
     window.location.href = "https://github.com/SivutraMIN"
 })
 
-const contact_button = document.getElementById("btn-contact")
+const contact_button = document.getElementById("../html/contact.html")
 contact_button.addEventListener("click", () => {
     try {
-        window.location.href = "./contact.html"
+        window.location.href = "./html/contact.html"
     }
     catch (error) {
         console.error("File not Found!")
